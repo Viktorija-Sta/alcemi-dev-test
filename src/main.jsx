9df6fe958399f14e8fc4da7c012acb840ac7cf06
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-08-12 17:37:56 +0300

    Reintroduce ActionButtons and MainAIWindow components with updated structure and functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage.js'
 import MainLayout from './layouts/MainLayout.js'
+import MainAIWindow from './components/AI-assistant/Main-window/MainAIWindow.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<MainPage />} />
+          <Route path='/AI-search' element={<MainAIWindow />} />
         </Route>
       </Routes>
      </BrowserRouter>
