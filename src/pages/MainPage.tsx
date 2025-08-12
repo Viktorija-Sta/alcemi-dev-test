e6b04dff776df08f4e39ec43a9af14ecdf3b0b0d
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-08-12 16:50:47 +0300

    Enhance MainPage layout with promotional banner and best sellers section; add AI search navigation

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,35 @@
 const MainPage: React.FC = () => {
     
     return (
-        <div>Main Page</div>
+        <>
+            <div className="main-image">
+                <img src="/src/assets/67a88cae6e72edc64d90dfacbbc96a25a9f7c7ff.png" alt="Main Banner" />
+                <p>Biggest discounts this season</p>
+                <nav>
+                    <a href="/sales">View sale</a>
+                </nav>
+            </div>
+
+            <div className="bottom-images">
+                <h2>BEST SELLERS</h2>
+               
+                <div className="image-container">
+
+                    <img src="/src/assets/67a88cae6e72edc64d90dfacbbc96a25a9f7c7ff.png" alt="" />
+               
+                    <img src="/src/assets/67a88cae6e72edc64d90dfacbbc96a25a9f7c7ff.png" alt="" />
+               
+                    <img src="/src/assets/67a88cae6e72edc64d90dfacbbc96a25a9f7c7ff.png" alt="" />
+
+                    <img src="/src/assets/6b3fb5e8f7fe3e2d05845e88db3a6a4b0c5d2324.png" alt="" />
+                    
+                    <nav>
+                        <a href="/AI-search">AI powered search <img src="/src/assets/fluent_search-sparkle-24-filled.svg" alt="" /></a>
+                    </nav>
+                </div>
+
+            </div>
+        </>
     )
 }
 
