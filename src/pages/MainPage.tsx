55982b4048306dc2da0d7990a04b13324fb301a5
Viktorija-Sta <viktorija.ribilyte@gmail.com>
2025-08-13 18:43:57 +0300

    refactor: add NavBar styles and integrate into the project

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection/HeroSection"
 import '../components/AI-assistant/AIWindow/AIWindow.scss'
 import '../components/HeroSection/HeroSection.scss'
 import '../components/BestSellerSection/BestSellerSection.scss'
+import '../components/NavBar/NavBar.scss'
 
 const MainPage: React.FC = () => {
     
